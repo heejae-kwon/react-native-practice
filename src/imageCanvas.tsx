53e4031069f5f1e7b277791c87f3b03f1c762453
Heejae Kwon <sfhy33@gmail.com>
2022-12-05 16:01:18 +0900

    last commit

--- src/imageCanvas.tsx
+++ src/imageCanvas.tsx
@@ -6,18 +6,16 @@ import {
     ScrollView,
     Text,
     Button,
-    View
+    View,
+    Image
 } from 'react-native';
 import { Divider } from 'react-native-paper';
 import Canvas, { Image as CanvasImage } from 'react-native-canvas';
 import { prepareAssets, readAsset } from './utils/assetsHelper';
+import { encode, decode } from 'base64-arraybuffer'
 
 
 function ImageCanvas() {
-    // get a list of files and directories in the main bundle
-    useEffect(() => {
-        prepareAssets()
-    }, []);
     // get a list of files and directories in the main bundle
 
     //const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -26,16 +24,30 @@ function ImageCanvas() {
     const [topResultConfidence, setConfidence] = useState("");
     const [inferenceTime, setInferenceTime] = useState("");
     const [inferenceTrigger, setinferenceTrigger] = useState(false);
+    const [imageSrc, setImageSrc] = useState("")
+    let imageData: Uint8ClampedArray = new Uint8ClampedArray()
 
+
+
+    // get a list of files and directories in the main bundle
+
+    useEffect(() => {
+        prepareAssets()
+        const getImageBase64 = async () => {
+            const imageBase64 = await readAsset('model/running_1.jpg', 'base64')
+            imageData = new Uint8ClampedArray(decode(imageBase64))
+            setImageSrc('data:image/jpeg;base64,' + imageBase64) //Image.resolveAssetSource(require('../assets/running_1.jpg')).uri
+            console.log(imageData)
+        }
+        getImageBase64()
+    }, [imageSrc]);
 
     // Load the image from the IMAGE_URLS array
-    const getImage = () => {
-        const sampleImageUrls: Array<{ text: string; value: string }> = IMAGE_URLS;
-        const random = Math.floor(Math.random() * (9 - 0 + 1) + 0);
-        return sampleImageUrls[random];
-    }
-    const [imageSrc, setImageSrc] = useState(getImage().value)
-    let imageData: Uint8ClampedArray = new Uint8ClampedArray()
+    /*    const getImage = () => {
+            const sampleImageUrls: Array<{ text: string; value: string }> = IMAGE_URLS;
+            const random = Math.floor(Math.random() * (9 - 0 + 1) + 0);
+            return sampleImageUrls[random];
+        }*/
 
     //   image.src = sampleImage.value;
 
@@ -88,14 +100,14 @@ function ImageCanvas() {
 
         image.crossOrigin = "Anonymous"
 
-        image.src = 'data:image/jpeg;base64,' + await readAsset('model/running_1.jpg', 'base64') //Image.resolveAssetSource(require('../assets/running_1.jpg')).uri
+
 
         image.addEventListener('load', () => {
             context.drawImage(image, 0, 0, canvas.width, canvas.height);
             context.getImageData(0, 0, canvas.width, canvas.height).then((imgdd) => {
                 const ta = Object.entries(imgdd.data);
-                console.log(ta)
-               // imageData = new Uint8ClampedArray(ta)
+                //console.log(ta)
+                // imageData = new Uint8ClampedArray(ta)
             })
         });
     }
@@ -107,13 +119,13 @@ function ImageCanvas() {
                     title='버튼' />
             </View>
             <Divider />
-            {/*//<canvas ref={canvasRef} width={props.width} height={props.height} />
-            <Image
-                source={{ uri: imageSrc }}
-                style={{ width: 200, height: 200 }}
-            />
-            **/}
-            <Canvas ref={handleImageRect} />
+            {
+                imageSrc && <Image
+                    source={{ uri: imageSrc }}
+                    style={{ width: 300, height: 300 }}
+                />
+            }
+            {/*<Canvas ref={handleImageRect} /*/}
             <Text>{topResultLabel} {topResultConfidence}</Text>
             <Text>{inferenceTime}</Text>
         </ScrollView>
